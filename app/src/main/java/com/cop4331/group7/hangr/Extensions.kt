71677ad1324c05d319f6663e37cc9a40c9b60c7b
Autumn Esponda <autumnskies1096@gmail.com>
2019-03-04 00:19:54 -0500

    register page works with validation; fix anim when pressing back in toolbar

--- app/src/main/java/com/cop4331/group7/hangr/Extensions.kt
+++ app/src/main/java/com/cop4331/group7/hangr/Extensions.kt
@@ -6,3 +6,9 @@ fun View.hideKeyboard() {
     val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
     imm.hideSoftInputFromWindow(windowToken, 0)
 }
+
+fun View.showKeyboard() {
+    this.requestFocus()
+    val inputMethodManager = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+    inputMethodManager.showSoftInput(this, InputMethodManager.SHOW_IMPLICIT)
+}
